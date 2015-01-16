71acc2dfd51275de8ea6f378532fdc895b6e6db3
Nate Buchan <natebuchan@gmail.com>
2015-01-16 10:03:37 -0800

    Rolled Back
    Rolled back to the sound project i was working on.

--- p5Testing.js
+++ p5Testing.js
@@ -1,80 +1,3 @@
-var system;
-
-function setup() {
-  createCanvas(720, 400);
-  system = new ParticleSystem(createVector(width/2, 50));
-}
-
-function draw() {
-  background(51);
-  system.addParticle();
-  system.run();
-}
-
-// A simple Particle class
-var Particle = function(position) {
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
-  this.position = position.get();
-  this.lifespan = 255.0;
-};
-
-Particle.prototype.run = function() {
-  this.update();
-  this.display();
-};
-
-// updates where the partical is going to be displayed and where it is located on the screen.
-Particle.prototype.update = function(){
-  this.velocity.add(this.acceleration);
-  this.position.add(this.velocity);
-  this.lifespan -= 2;
-};
-
-// function to display the particals or small circles
-Particle.prototype.display = function() {
-  stroke(200, this.lifespan);
-  strokeWeight(2);
-  fill(127, this.lifespan);
-  ellipse(this.position.x, this.position.y, 12, 12);
-};
-
-// checks to see if the partical is still needed to be displayed and if not it will be removed
-Particle.prototype.isDead = function(){
-  if (this.lifespan < 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-// the variable that defines the system in which the particals are displayed
-var ParticleSystem = function(position) {
-  this.origin = position.get();
-  this.particles = [];
-};
-
-ParticleSystem.prototype.addParticle = function() {
-  this.particles.push(new Particle(this.origin));
-};
-
-ParticleSystem.prototype.run = function() {
-  for (var i = this.particles.length-1; i >= 0; i--) {
-    var p = this.particles[i];
-    p.run();
-    if (p.isDead()) {
-      this.particles.splice(i, 1);
-    }
-  }
-};
-
-
-
-
-
-
-
-/*
 var song;
 
 function setup() {
@@ -92,7 +15,7 @@ function mousePressed() {
     background(0, 255, 0);
   }
 }
-*/
+
 
 /* This works on its own
 function preload() {
